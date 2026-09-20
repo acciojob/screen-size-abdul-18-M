78e96810adac27920cc31edc4e07b981317dc16c
Abdul Mulla <abdulmulla8071@gmail.com>
2026-09-20 21:52:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const sizeInfo = document.querySelector("#sizeInfo");
+
+function updateSize() {
+    const h1 = document.createElement("h1");
+
+    h1.innerHTML = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+
+    sizeInfo.innerHTML = "";
+    sizeInfo.appendChild(h1);
+}
+
+window.addEventListener("resize", updateSize);
+
+updateSize();
